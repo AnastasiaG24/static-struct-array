0d9a85b23412c19a00d3823b71ab6219872bcc9c
AnastasiaG24 <gandacov.anastasia@gmail.com>
2025-05-08 00:00:15 +0300

    added changes

--- 1d_array_static_allocation.c
+++ 1d_array_static_allocation.c
@@ -103,3 +103,178 @@ Country inputNewCountry() {
 	scanf_s("%i", &newCountry.area);
 	printf("President's name: ");
 	scanf_s("%s", newCountry.presidentName, (unsigned)_countof(newCountry.presidentName));
+	return newCountry;
+}
+
+void insertAtEnd(Country** countries, int* count, Country newCountry) {
+	(*count)++;
+	Country* temp = realloc(*countries, (*count) * sizeof(Country));
+	if (!temp) {
+		printf("Memory allocation failed.\n");
+		exit(1);
+	}
+	*countries = temp;
+	(*countries)[*count - 1] = newCountry;
+}
+
+void insertAtBeginning(Country** countries, int* count, Country newCountry) {
+	(*count)++;
+	Country* temp = realloc(*countries, (*count) * sizeof(Country));
+	if (!temp) {
+		printf("Memory reallocation failed.\n");
+		exit(1);
+	}
+	*countries = temp;
+	for (int i = *count - 1; i > 0; i--) {
+		(*countries)[i] = (*countries)[i - 1];
+	}
+	(*countries)[0] = newCountry;
+}
+
+void insertAtPosition(Country** countries, int* count, Country newCountry, int position) {
+	if (position < 0 || position > *count) {
+		printf("Invalid position.\n");
+		return;
+	}
+	(*count)++;
+	Country* temp = realloc(*countries, (*count) * sizeof(Country));
+	if (!temp) {
+		printf("Memory reallocation failed.\n");
+		exit(1);
+	}
+	*countries = temp;
+	for (int i = *count - 1; i > position; i--) {
+		(*countries)[i] = (*countries)[i - 1];
+	}
+	(*countries)[position] = newCountry;
+}
+
+void deleteAtPosition(Country** countries, int* count, int position) {
+	for (int i = position - 1; i < *count - 1; i++) {
+		(*countries)[i] = (*countries)[i + 1];
+	}
+	(*count)--;
+	Country* temp = realloc(*countries, (*count) * sizeof(Country));
+	if (!temp && *count > 0) {
+		printf("Memory reallocation failed after deletion.\n");
+		exit(1);
+	}
+	*countries = temp;
+	printf("Country at position %i has been deleted.\n", position);
+}
+
+int main() {
+	int count, menuOption, searchOption, sortOption;
+	char name[100], capital[100], president[100];
+	int cities, year, area;
+
+	printf("Enter number of countries: ");
+	scanf_s("%i", &count);
+
+	Country* countries = (Country*)malloc(count * sizeof(Country));
+	if (!countries) {
+		printf("Memory allocation error.\n");
+		return 1;
+	}
+
+	readData(count, countries);
+	displayData(count, countries);
+
+	printf("\nMenu:\n");
+	printf("1 - Search by field\n");
+	printf("2 - Sort by field\n");
+	printf("3 - Insert at end\n");
+	printf("4 - Insert at beginning\n");
+	printf("5 - Insert at position\n");
+	printf("6 - Delete by position\n");
+	printf("Choose an option: ");
+	scanf_s("%i", &menuOption);
+
+	switch (menuOption) {
+	case 1:
+		printf("Search by:\n1-Name 2-Capital 3-Cities 4-Year 5-Area 6-President\nChoose: ");
+		scanf_s("%i", &searchOption);
+		switch (searchOption) {
+		case 1:
+			printf("Enter country name: ");
+			scanf_s("%s", name, (unsigned)sizeof(name));
+			searchByWord(count, countries, name); break;
+		case 2:
+			printf("Enter capital: ");
+			scanf_s("%s", capital, (unsigned)sizeof(capital));
+			searchByWord(count, countries, capital); break;
+		case 3:
+			printf("Enter number of cities: ");
+			scanf_s("%i", &cities);
+			searchByNumber(count, countries, cities); break;
+		case 4:
+			printf("Enter foundation year: ");
+			scanf_s("%i", &year);
+			searchByNumber(count, countries, year); break;
+		case 5:
+			printf("Enter area: ");
+			scanf_s("%i", &area);
+			searchByNumber(count, countries, area); break;
+		case 6:
+			printf("Enter president's name: ");
+			scanf_s("%s", president, (unsigned)sizeof(president));
+			searchByWord(count, countries, president); break;
+		} break;
+
+	case 2:
+		printf("Sort by:\n1-Name 2-Capital 3-Cities 4-Year 5-Area 6-President\nChoose: ");
+		scanf_s("%i", &sortOption);
+		switch (sortOption) {
+		case 1: qsort(countries, count, sizeof(Country), compareCountryName); break;
+		case 2: qsort(countries, count, sizeof(Country), compareCapital); break;
+		case 3: qsort(countries, count, sizeof(Country), compareCityCount); break;
+		case 4: qsort(countries, count, sizeof(Country), compareFoundationYear); break;
+		case 5: qsort(countries, count, sizeof(Country), compareArea); break;
+		case 6: qsort(countries, count, sizeof(Country), comparePresidentName); break;
+		}
+		printf("\nSorted list:\n");
+		displayData(count, countries);
+		break;
+
+	case 3:
+		insertAtEnd(&countries, &count, inputNewCountry());
+		displayData(count, countries);
+		break;
+
+	case 4:
+		insertAtBeginning(&countries, &count, inputNewCountry());
+		displayData(count, countries);
+		break;
+
+	case 5: {
+		int pos;
+		printf("Enter position: ");
+		scanf_s("%i", &pos);
+		if (pos >= 0 && pos <= count) {
+			insertAtPosition(&countries, &count, inputNewCountry(), pos);
+			displayData(count, countries);
+		}
+		else {
+			printf("Invalid position.\n");
+		}
+		break;
+	}
+
+	case 6: {
+		int pos;
+		printf("Enter position to delete: ");
+		scanf_s("%i", &pos);
+		if (pos >= 1 && pos <= count) {
+			deleteAtPosition(&countries, &count, pos);
+			displayData(count, countries);
+		}
+		else {
+			printf("Invalid position.\n");
+		}
+		break;
+	}
+	}
+
+	free(countries);
+	return 0;
+}
